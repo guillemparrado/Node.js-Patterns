76eb97fe238094bf7c3c0e519389ba1f52294e6c
Guillem Parrado <guillempado@gmail.com>
2023-05-23 18:27:39 +0200

    Millora 2.1

--- 2.1 Middleware/Calculadora.js
+++ 2.1 Middleware/Calculadora.js
@@ -3,12 +3,14 @@ FONTS:
  https://javascript.plainenglish.io/basic-middleware-pattern-in-javascript-ef8756a75cb1
 
  */
+
 class Calculadora {
     constructor(operacions){
-        this.functions = new Map();
-        this.functions.set('suma', (left, right) => left + right);
-        this.functions.set('resta', (left, right) => left - right);
-        this.functions.set('multiplicació', (left, right) => left * right);
+        this.functions = {
+            "suma": (left, right) => left + right,
+            "resta": (left, right) => left - right,
+            "multiplicació": (left, right) => left * right
+        }
         this.middlewares = [];
         this.operacions = operacions;
     }
@@ -29,7 +31,7 @@ class Calculadora {
                 console.log(op);
             }
             // Executa càlcul
-            const executaOperacio = this.functions.get(op['operation']);
+            const executaOperacio = this.functions[op['operation']];
             op['result'] = executaOperacio(op['left'], op['right']);
             console.log(`Resultat operació:`);
             console.log(op);
